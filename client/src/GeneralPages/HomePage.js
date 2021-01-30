bb12d9ce1e1344706b40fa603b2e1f053161d9ea
ISTANBULBEKLE <ekipkalir@gmail.com>
2021-01-30 21:31:28 +0000

    formatting addded

--- client/src/GeneralPages/HomePage.js
+++ client/src/GeneralPages/HomePage.js
@@ -5,28 +5,28 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
 
 	return (
-    <div className="container">
-      <div className="homepage">
-        <h1 className="welcome-text-h1 text-center">Welcome to CYF Quiz App</h1>
-        <div className="homepage-access">
-          <div className="homepage-student-access">
-            <Link to="/studentlogin">
-              <button className="btn-lg btn-danger text-wrap welcome-text">
+		<div className="container">
+			<div className="homepage">
+				<h1 className="welcome-text-h1 text-center">Welcome to CYF Quiz App</h1>
+				<div className="homepage-access">
+					<div className="homepage-student-access">
+						<Link to="/studentlogin">
+							<button className="btn-lg btn-danger text-wrap welcome-text">
                 Student Login
-              </button>
-            </Link>
-          </div>
-          <div className="homepage-mentor-access">
-            <Link to="/mentorlogin">
-              <button className="btn-lg btn-danger text-wrap welcome-text">
+							</button>
+						</Link>
+					</div>
+					<div className="homepage-mentor-access">
+						<Link to="/mentorlogin">
+							<button className="btn-lg btn-danger text-wrap welcome-text">
                 Mentor Login
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+							</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
